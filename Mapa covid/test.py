c0b6fead72021353be33880d8d8ce59f24badcaa
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-11-23 10:02:23 -0300

    up calendar

---
+++ Mapa covid/test.py
@@ -0,0 +1,8 @@
+p1 = float(input('N1: ').replace("1", "5000"))
+p2 = float(input('N2: '))
+p3 = float(input('N3: '))
+p4 = float(input('N4: '))
+
+media = (p1 + p2 + p3 + p4) / 4
+
+print (f'As notas foram {p1}, {p2}, {p3} e {p4}' f', sendo a media das notas foram: {media}' )
